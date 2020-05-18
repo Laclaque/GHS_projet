8157d768b0e7cb2ac327cbb1024ccdc666abbe1f
lag <lag@debian-serv>
2020-05-18 17:58:43 +0200

    graph depuis fichier et liste d'adjacence depuis graphe

--- ghs.py
+++ ghs.py
@@ -1,6 +1,10 @@
 #!/bin/python3
 
 def find_min_edge_in_list(edges_list):
+    result = edges_list[0]
+    for node in edges_list:
+        if node["value"]<result["value"]:
+            result=node
 
 
 def bloc_init(edges_list):
---
+++ graphs.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+#files must be be formated as follows :
+###
+#nbOfVertices
+#node1 node2 weightOnTheEdge
+#node1 node3 weightOnTheEdge
+#node2 node1 weightOnTheEdge
+#node3 node1 weightOnTheEdge
+#...
+###
+#the file must contain every edge two times as it is considered as lists of neighbors
+# in our example node1 has 2neigbors whereas node2 and node3 have only 1 neighbor each
+
+#to comment
+#not checked yet
+def read_graph_from_file(fname):
+    graph = {
+            "size" : 0,
+            "edges" : {}
+            }
+    with open(fname, 'r') as f_:
+        #first, get the size
+        graph["size"] = f_.readline()
+        #then, every line contains an edge
+        for line in f_:
+            splitLine = line.split()
+            if splitLine[0] not in graph["edges"]:
+                #here we discovered another vertice name so add a new adjacency list (dictionnary)
+                graph["edges"][splitLine[0]] = {}
+            #add the edge to the graph
+            graph["edges"][splitLine[0]][splitLine[1]] = splitLine[2]
+    return graph
+
+
+#to comment
+#not checked yet
+def get_adjacencyList_from_graph(graph, node):
+    return graph["edges"][node]
+
+
