77984eccd9163521fade4afdabc072c497e9df17
laclaque <laclaque@test.com>
2020-05-20 20:09:45 +0200

    firsts blocs

---
+++ exchanges_queues.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+def send_to(q_,receiver,type,val1,val2,val3)
+	#use the correct queue to send (q_ is a set to define)
+	#write in queue
+
+def recv_from(q_,i):
+	#use the correct queue to receive
+	#read in queue
+	return {"type" : t, "L" : L, "F" : F, "S" : S} #pour la forme
--- ghs.py
+++ ghs.py
@@ -1,10 +1,41 @@
 #!/bin/python3
 
-def find_min_edge_in_list(edges_list):
-    result = edges_list[0]
-    for node in edges_list:
-        if node["value"]<result["value"]:
-            result=node
+def find_min_edgeKey_in_adjlist(edgesList):
+	#verifier si c'est possible de retourner un tuple, sinon garder seulement la
+	minimalEdgeKey = min(edgesList, key=edgesList.get)
+	return minimalEdgeKey 
 
+def bloc_init(edgesList,q_,i):
+	# //!\\ vérifier la portée des variables en python
+	#de toute façon ce bloc n'est appelé qu'une seule fois donc on pourra le mettre dans le code si besoin
+	set_ = {
+		"i" : i,
+		 "canal" : {},
+		"niv" : 0,
+		"etat" : "found",
+		"recu" : 0,
+		"nom" : None,
+		"pere" : None,
+		"mcan" : None,
+		"testcan" : None,
+		"mpoids" : None,
+		"edges" : edgesList,
+		"queues" : q_
+	}
+	for neighbor in edgesList:
+		set_["canal"][neighbor] = "basic"
+	set_["canal"][find_min_edgeKey_in_adjlist(edgesList)] = "branch"
+	send_to(set_["queues"],minimalEdge,"connect",0, None, None)
+	return set_
 
-def bloc_init(edges_list):
+def bloc_connect(L,j,set_):
+	# //!\\vérifier la portée des variables en python
+	if L < set_["niv"]:
+		set_["canal"][j] = "branch"
+		send_to(set["queues"],j,"initiate",set_["niv"],set_["nom"],set_["etat"])
+	elif set_["canal"][j] = "basic":
+		send_to(set["queues"],set["i"],"connect",L,None,None) #traiter le message plus tard
+	else:
+		send_to(set["queues"],j,"initiate",set_["niv"]+1,set_["edges"][j],"find")
+		
+	return set_
